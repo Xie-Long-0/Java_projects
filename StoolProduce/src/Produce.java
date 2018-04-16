1c227b3d7545b7313741dbb207c6a2ee59f99497
XieLong <1293336945@qq.com>
2018-04-16 20:18:13 +0800

    change codes

--- StoolProduce/src/Produce.java
+++ StoolProduce/src/Produce.java
@@ -13,31 +13,44 @@ public class Produce
 	static void test()
 	{
 		System.out.printf("本作坊每周工作5天\n"
-			+ "每天生产圆柱凳子10件，组合凳子5件\n\n"
-			+ "现设圆柱凳子的半径为 10cm，高为 20cm；\n"
-			+ "设组合凳子的顶部圆柱半径为 12cm，高为 5cm，\n"
-			+ "中部圆柱半径为 5cm，高为 15cm，\n"
-			+ "底部圆柱半径为 8cm，高为 5cm。\n\n");
+			+ "每天生产圆柱凳子10件，组合凳子5件\n\n");
 
-		Cylinder cylstool = new Cylinder(10, 20);
-		Stool comstool = new Stool(12, 5, 5, 15, 8, 5);
+		int[][] data1 = {{20, 15}, {18, 12}, {12, 20}, {25, 8}};
 
-		double area_of_cylstool = cylstool.getArea();
-		double volume_of_cylstool = cylstool.getVolume();
-		double area_of_comstool = comstool.getArea();
-		double volume_of_comstool = comstool.getVolume();
+		int[][] data2 = {{10, 5, 18, 12, 5, 20},
+						 {15, 20, 14, 8, 8, 25},
+						 {6, 15, 2, 20, 30, 15},
+						 {20, 8, 15, 5, 30, 10}};
 
-		System.out.printf("则生产一件圆柱凳子需要油漆（表面积）：%.3f cm2\n"
-			+ "需要木料（体积）：%.3f cm3\n", area_of_cylstool, volume_of_cylstool);
-		System.out.printf("生产一件组合凳子需要油漆（表面积）：%.3f cm2\n"
-			+ "需要木料（体积）：%.3f cm3\n\n", area_of_comstool, volume_of_comstool);
+		Cylinder cylstool;
+		Stool comstool;
 
-		double area_per_day = area_of_cylstool*10 + area_of_comstool*5;
-		double volume_per_day = volume_of_cylstool*10 + volume_of_comstool*5;
+		for (int n = 0; n < data1.length; n++)
+		{
+			cylstool = new Cylinder(data1[n][0], data1[n][1]);
+			comstool = new Stool(data2[n][0], data2[n][1],
+					data2[n][2], data2[n][3], data2[n][4], data2[n][5]);
 
-		System.out.printf("工作一天需要油漆：%.3f cm2\n"
-			+ "工作一天需要木料：%.3f cm3\n\n", area_per_day, volume_per_day);
-		System.out.printf("工作一周需要油漆：%.3f cm2\n"
-			+ "工作一周需要木料：%.3f cm3\n\n", area_per_day*5, volume_per_day*5);
+			System.out.print("圆柱凳子数据：");
+			for (int i : data1[n])
+				System.out.print(i + ", ");
+			System.out.println();
+			System.out.print("组合凳子数据：");
+			for (int i : data2[n])
+				System.out.print(i + ", ");
+			System.out.println();
+
+			double area_of_cylstool = cylstool.getArea();
+			double volume_of_cylstool = cylstool.getVolume();
+			double area_of_comstool = comstool.getArea();
+			double volume_of_comstool = comstool.getVolume();
+
+			double area_per_day = area_of_cylstool*10 + area_of_comstool*5;
+			double volume_per_day = volume_of_cylstool*10 + volume_of_comstool*5;
+
+			System.out.printf("每周需要油漆：%.3f\n"
+				+ "每周需要木料：%.3f\n\n", area_per_day*5, volume_per_day*5);
+
+		}
 	}
 }
