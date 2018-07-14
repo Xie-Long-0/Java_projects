8c1192f84269cb921263fca194f46f0793a5ecf8
XieLong <1293336945@qq.com>
2018-07-14 14:31:32 +0800

    add new projects

---
+++ Java_IO/src/Main.java
@@ -0,0 +1,123 @@
+import student.*;
+
+import java.io.*;
+import java.text.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SimpleDateFormat std = new SimpleDateFormat("yyyy-MM-dd");
+
+		Student[] students = new Student[3];
+		try {
+			students[0] = new Student(101, "XieLiu", std.parse("1997-02-01"));
+			students[1] = new Student(102, "LiuOri", std.parse("1996-05-12"));
+			students[2] = new Student(106, "Promise", std.parse("1998-10-25"));
+		} catch (ParseException pe) {
+			pe.printStackTrace();
+		}
+
+		List<Student> stu_list;
+
+		System.out.println("--字符流读写--");
+		writeStudentsToTextFile(students);
+		stu_list = readStudentsFromTextFile();
+		printList(stu_list);
+
+		System.out.println("--字节流读写--");
+		writeStudentsToBinFile(stu_list);
+		stu_list = readStudentsFromBinFile();
+		printList(stu_list);
+
+	}
+
+	//字符流读写
+	static void writeStudentsToTextFile(Student[] _students) {
+		try {
+			File txtFile = new File("student.txt");
+			BufferedWriter buffWriter = new BufferedWriter(new FileWriter(txtFile));
+
+			for (int i = 0; i < _students.length; i++) {
+				buffWriter.write(_students[i].toString());
+				buffWriter.newLine();
+			}
+
+			buffWriter.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	static List<Student> readStudentsFromTextFile() {
+
+		SimpleDateFormat std = new SimpleDateFormat("yyyy-MM-dd");
+		List<Student> stu_list = new ArrayList<>();
+
+		try {
+			File txtFile = new File("student.txt");
+			BufferedReader buffReader = new BufferedReader(new FileReader(txtFile));
+
+			while (buffReader.ready()) {
+				//拆分字符串，重构Student
+				String[] s = buffReader.readLine().split(",", 3);
+				int id = Integer.parseInt(s[0]);
+				String name = s[1];
+				Date birth = std.parse(s[2]);
+				stu_list.add(new Student(id, name, birth));
+			}
+
+			buffReader.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return stu_list;
+	}
+
+	//字节流读写
+	static void writeStudentsToBinFile(List<Student> _stu_list) {
+		try {
+			FileOutputStream binFile = new FileOutputStream("student.bin");
+			ObjectOutputStream oos = new ObjectOutputStream(binFile);
+
+			for (int i = 0; i < _stu_list.size(); i++) {
+				oos.writeObject(_stu_list.get(i));
+			}
+
+			oos.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	static List<Student> readStudentsFromBinFile() {
+		List<Student> stu_list = new ArrayList<>();
+
+		try {
+			FileInputStream binFile = new FileInputStream("student.bin");
+			ObjectInputStream ois = new ObjectInputStream(binFile);
+
+			while (ois.available() > 1) {
+				stu_list.add((Student)ois.);
+			}
+
+			ois.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return stu_list;
+	}
+
+	static void printList(List<Student> _stu_list) {
+		for (int i = 0; i < _stu_list.size(); i++) {
+			_stu_list.get(i).show();
+		}
+	}
+
+}
--- Java_SQL/src/myjavasql/MyJavaSQL.java
+++ Java_SQL/src/myjavasql/MyJavaSQL.java
@@ -3,48 +3,50 @@ import java.sql.*;
 
 public class MyJavaSQL {
 
-	public static void main(String[] args) {
+	String host;
+	String user;
+	String password;
+	Connection link;
 
-		String host = "jdbc:sqlserver://10.64.70.117; DatabaseName=MydbForJava;";
-		String user = "dbtester";
-		String passwd = "dbtester";
+	// 无参构造函数
+	public MyJavaSQL() {
+		host = "jdbc:sqlserver://localhost; DatabaseName=master;";
+		user = "sa";
+		password = "123";
+		connect();
+	}
 
+	// 带参构造函数
+	public MyJavaSQL(String _host, String _user, String _password) {
+		host = _host;
+		user = _user;
+		password = _password;
+		connect();
+	}
+
+	// 连接数据库
+	public void connect() {
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 			System.out.println("驱动加载成功");
 		} catch (Exception e) {
 			e.printStackTrace();
-			return;
+			System.exit(-1);
 		}
 
-		Connection link = getConnection(host, user, passwd);
-
-		String sqlinsert = "INSERT INTO Students(classno,sno,name,birthdate) VALUES(1601,129,'谢耀辉','1997-02-03')";
-		//mySqlInsert(link, sqlinsert);
-		String sqlupdate = "UPDATE Students SET name='xjbl', birthdate='2000-01-01' WHERE classno=20 AND sno=22";
-		//mySqlUpdate(link, sqlupdate);
-		String sqldelete = "DELETE FROM Students WHERE classno=1 AND sno=29";
-		//mySqlDelete(link, sqldelete);
-		String sqlquery = "SELECT sno,classno,name,birthdate FROM Students";
-		mySqlQuery(link, sqlquery);
-	}
-
-	public static Connection getConnection(String _host, String _user, String _passwd) {
-		Connection link = null;
-
 		try {
-			link = DriverManager.getConnection(_host, _user, _passwd);
+			link = DriverManager.getConnection(host, user, password);
 			System.out.println("数据库连接成功：" + link.getCatalog());
 		} catch (SQLException e) {
 			e.printStackTrace();
 			System.exit(-1);
 		}
-		return link;
 	}
 
-	public static void mySqlInsert(Connection _link, String _sql) {
+
+	public void insert(String _sql) {
 		try {
-			Statement stmt = _link.createStatement();
+			Statement stmt = link.createStatement();
 			int rs = stmt.executeUpdate(_sql);
 			System.out.println("--inserted: " + rs);
 			stmt.close();
@@ -53,9 +55,9 @@ public class MyJavaSQL {
 		}
 	}
 
-	public static void mySqlQuery(Connection _link, String _sql) {
+	public void query(String _sql) {
 		try {
-			Statement stmt = _link.createStatement();
+			Statement stmt = link.createStatement();
 			ResultSet result = stmt.executeQuery(_sql);
 			handleResults(result);
 			result.close();
@@ -65,9 +67,9 @@ public class MyJavaSQL {
 		}
 	}
 
-	public static void mySqlUpdate(Connection _link, String _sql) {
+	public void update(String _sql) {
 		try {
-			Statement stmt = _link.createStatement();
+			Statement stmt = link.createStatement();
 			int rs = stmt.executeUpdate(_sql);
 			System.out.println("--updated: " + rs);
 			stmt.close();
@@ -76,9 +78,9 @@ public class MyJavaSQL {
 		}
 	}
 
-	public static void mySqlDelete(Connection _link, String _sql) {
+	public void delete(String _sql) {
 		try {
-			Statement stmt = _link.createStatement();
+			Statement stmt = link.createStatement();
 			int rs = stmt.executeUpdate(_sql);
 			System.out.println("--deleted: " + rs);
 			stmt.close();
@@ -87,17 +89,28 @@ public class MyJavaSQL {
 		}
 	}
 
-	public static void handleResults(ResultSet rs) throws SQLException {
-		while (rs.next()) {
-			String classno = rs.getString("classno");
-			String sno = rs.getString("sno");
-			String name = rs.getString("name");
-			String birthday = rs.getString("birthdate");
+	private void handleResults(ResultSet rs) throws SQLException {
 
-			System.out.println(" classno: " + classno);
-			System.out.println("     sno: " + sno);
-			System.out.println("    name: " + name);
-			System.out.println("birthday: " + (birthday==null ? "<NULL>" : birthday.substring(0,10)));
+		// 取出ResultSet的MetaData
+		ResultSetMetaData rsmd = rs.getMetaData();
+
+		// 遍历结果集
+		while (rs.next()) {
+			// 对每个结果集遍历所有的列
+			for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+
+				// 取得列名
+				String columnName = rsmd.getColumnName(i);
+				// 取得列的内容
+				String columnData = rs.getString(i);
+
+				if (columnName == "birthdate") {
+					columnData = columnData.substring(0, 10);
+				}
+
+				System.out.println(columnName + ": \t" + (columnData == "" ? "<NULL>" : columnData));
+
+			}
 			System.out.println();
 		}
 	}
